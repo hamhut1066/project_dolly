47d3640c57fb236f07a6da1ac25df1eead608f5c
Chris Swart <windupcat2012@gmail.com>
2013-10-26 19:34:54 +0100

    trying to implement user reg is user.py

--- src/server/user.py
+++ src/server/user.py
@@ -1,4 +1,13 @@
+import Database
+
 class User:
-	def __init__(name, email):
+	def __init__(self, name, email):
 		self.name = name
 		self.email = email
+
+	def user_creation(self, db):
+		"""user creation returns error string if unsuccessfull"""
+		if (db.query("select '%s' from db") % (self.name)) != self.name):
+			db.update("insert '%s") % (self.name)
+		else:
+			return "Name already exists!! Please input another name:"
