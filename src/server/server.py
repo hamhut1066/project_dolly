85d8e49c0a0e8daef5a7e82ae248b269e7170a83
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-26 14:37:32 -0400

    add server to server-side

---
+++ src/server/server.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import os
+
+#Create custom HTTPRequestHandler class
+class KodeFunHTTPRequestHandler(BaseHTTPRequestHandler):
+    
+    #handle GET command
+    def do_GET(self):
+        rootdir = './' #file location
+        try:
+            #f = open(rootdir + self.path) #open requested file
+
+            #send code 200 response
+            self.send_response(200)
+
+            #send header first
+            self.send_header('Content-type','text-html')
+            self.end_headers()
+
+            #send file content to client
+            self.wfile.write(self.path)
+            #f.close()
+            #output log for sucessful connection
+            print "connection from ", self.client_address, " sucessful" 
+            return
+            
+        except IOError:
+            self.send_error(404, 'file not found')
+            print("connection failed")
+    
+def run():
+    print('http server is starting...')
+
+    #ip and port of servr
+    #by default http server port is 80
+    server_address = ('127.0.0.1', 5000)
+    httpd = HTTPServer(server_address, KodeFunHTTPRequestHandler)
+    print('http server is running...')
+    httpd.serve_forever()
+    
+if __name__ == '__main__':
+    run()
