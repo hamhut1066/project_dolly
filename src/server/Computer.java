2f90ec94b28daf70549d98a6a6c0e0cf1de37d2a
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-26 11:11:08 -0400

    trying to get db working

---
+++ src/server/Computer.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author hamhut
+ *
+ */
+public class Computer {
+
+	/**
+	 * This class represents a single computer owned by a user and running multiple processes
+	 */
+	
+	//variable definitions
+	private String name = null;
+	private Ip ip = new Ip(null);
+	
+	
+	public Computer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/server/Ip.java
@@ -0,0 +1,13 @@
+package server;
+
+public class Ip {
+
+	/* this class is used to easily manipulate and use Ip addresses */
+	
+	private String ip = null;
+	public Ip(String ip) {
+		// TODO Auto-generated constructor stub
+		this.ip = ip;
+	}
+
+}
