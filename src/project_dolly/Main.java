9eb9c05788af1f9f107b5f45edf06de01d2782ea
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-26 09:47:50 -0400

    add client, server packages

--- src/project_dolly/Main.java
+++ src/project_dolly/Main.java
@@ -14,6 +14,8 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// this should only execute the server and the client...
+		// both parts of the program (client/server) should be easily separated
 
 	}
 
