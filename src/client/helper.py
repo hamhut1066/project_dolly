28614e8d40630e921eda37f0b88ef28b5ee34960
Aseem Narang <aseemnarang17@yahoo.com>
2013-10-27 13:10:17 +0000

    helper file

---
+++ src/client/helper.py
@@ -0,0 +1,22 @@
+class help
+    def helper_menu(self)
+        print """ welcome to the helper menu!
+                       1: help with login
+                       2:help with gameplay
+                       3:help with something else"
+                    """
+        choice = input( "give your choice")
+
+        if input == 1 :
+            print "you have been helped with login
+        elif input == 2: 
+            print "you have been helped with gameplay "
+        elif input == 3
+            print "you have been helped with something else "
+
+        print "enjoy your game"
+
+        
+            
+    
+        
