881affb035d8a59080d679f41c374df8b9ed39dc
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-27 08:14:24 -0400

    add static username for testing

--- src/server/evaluate.py
+++ src/server/evaluate.py
@@ -4,9 +4,13 @@ class Evaluate:
     def __init__(self):
         pass
 
+    #this command evaluates what the client wants and proceeds from there
     def read(self, msg):
         pre = msg.split(":")
-        if pre[0] == "username":
-            return "login"
+        if pre[0] == "login" and pre[1] == "aoeu":
+            #this will be used if the user is logging in
+            return "true"
         else:
-            return 
+            #this will check that the user is logging in otherwise it will do
+            #something random
+            return pre
