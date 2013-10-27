cc15b7f2c396ca557d46f603b7af059a87463e20
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-27 09:11:09 -0400

    add user login test

--- src/server/evaluate.py
+++ src/server/evaluate.py
@@ -1,3 +1,4 @@
+from database import *
 
 class Evaluate:
 
@@ -7,10 +8,16 @@ class Evaluate:
     #this command evaluates what the client wants and proceeds from there
     def read(self, msg):
         pre = msg.split(":")
-        if pre[0] == "login" and pre[1] == "aoeu":
-            #this will be used if the user is logging in
-            return "true"
-        else:
-            #this will check that the user is logging in otherwise it will do
-            #something random
-            return pre
+        con = Database("test.db")
+        tmp = con.query("SELECT name FROM tbl_users WHERE name == '%s'" % pre[1])
+        if pre[0] == "login":
+            if tmp is not None:
+                return tmp[0]
+                #this will be used if the user is logging in
+                #need to connect to the database and check if the user exists
+                #return "true"
+            else:
+                #returns as a guest user if the username does not exist
+                #something random
+                return "guest"
+
