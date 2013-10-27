fb41312d0d852bdc14e044285c8141e84f8aad9c
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-27 10:14:01 -0400

    change logic for menu system

--- src/client/main.py
+++ src/client/main.py
@@ -2,40 +2,50 @@
 
 from assign import *
 from connect import *
+from menu import *
 
 con = Connect()
 prompt = ">"
+username = "guest"
 
 while 1:
     user = AssignValues()
     data = "login:" + user.name
-    return_val = con.send(data)
-    if return_val == "true":
+    username = con.send(data)
+    if username != "guest":
         #the user exists and he can start playing
         break
     print return_val
 #they now reach a menu system that gives them a set of options
 
 print '''
-Welcome to Project Dolly!!!
+Welcome %s, to Project Dolly!!!
 
-Please feel free to have a look around
-type help for more information
-'''
-
+Please feel free to have a look around.
+type [h]elp for more information
+''' % username
+men = Menu()
+functions = {
+        'h'    : men.helper_menu,
+        'H'    : men.helper_menu,
+        'help' : men.helper_menu,
+        'ls'   : men.helper_menu
+        }
 #this is the menu system
 while 1:
     print prompt,
     userin = raw_input()
-    #if statements for every condition
+    #if statement to quit the game if needed
     if userin == "q" or userin == "Q" or userin == "quit":
         break
-    elif userin == "help":
-        print "you have been helped"
-    elif userin == "ls":
-        print "you have 2 computers"
+    #a dictionary of functions to call depending on user input
     else:
-        print "command not found, type help for more information"
+        #using try-catch to stop invalid options from crashing the program
+        try:
+            func = functions[userin]
+            func()
+        except:
+            print "Command not Found, type h for help"
 
 
 print "goodbye!"
