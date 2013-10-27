c376aaeff2d3ed9640feae79a0b0353ca6b1013d
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-27 08:14:06 -0400

    start creation of menu system

--- src/client/main.py
+++ src/client/main.py
@@ -4,8 +4,36 @@ from assign import *
 from connect import *
 
 con = Connect()
-user = AssignValues()
+prompt = ">"
 
-data = "username:" + user.name
-print data
-print con.send(data)
+while 1:
+    user = AssignValues()
+    data = "login:" + user.name
+    if con.send(data) == "true":
+        #the user exists and he can start playing
+        break
+#they now reach a menu system that gives them a set of options
+
+print '''
+Welcome to Project Dolly!!!
+
+Please feel free to have a look around
+type help for more information
+'''
+
+#this is the menu system
+while 1:
+    print prompt,
+    userin = raw_input()
+    #if statements for every condition
+    if userin == "q" or userin == "Q" or userin == "quit":
+        break
+    elif userin == "help":
+        print "you have been helped"
+    elif userin == "ls":
+        print "you have 2 computers"
+    else:
+        print "command not found, type help for more information"
+
+
+print "goodbye!"
