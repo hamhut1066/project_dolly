079e7390e8634e27b96097b118e9fbf5f9834560
Hamish Hutchings <hamhut1066@gmail.com>
2013-10-27 10:12:10 -0400

    fix menu to work better

--- src/client/menu.py
+++ src/client/menu.py
@@ -1,12 +1,24 @@
-import user
-import computer
-import port
-class menu
-    def menu_show
-        print ( "welcome to the penetration game")
-        choice = input( "chose option-  1:see network 2:add compter ")
-        if(choice == 1)
-            show_values.show_computers(computers)
-        else if (choice == 2)
-            assign_values.assign_computer(computers,check_compadd)
-        
+class Menu:
+    def __init__(self):
+        pass
+
+    def helper_menu(self):
+        while 1:
+            print """ 
+                    welcome to the helper menu!
+                    1: help with [l]ogin
+                    2:help with [g]ameplay
+                    3:help with s[o]mething else"
+                    """
+            choice = raw_input( "give your choice: "), 
+
+            if choice[0] == "l":
+                print "you have been helped with login"
+            elif choice[0] == "g": 
+                print "you have been helped with gameplay "
+            elif choice[0] == "o":
+                print "you have been helped with something else "
+            elif choice[0] == "q" or choice[0] == "b":
+                break
+            print "...",
+            raw_input()
