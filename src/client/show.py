d4292872186238c13bc68224d2171a1b26496af4
Aseem Narang <aseemnarang17@yahoo.com>
2013-10-27 10:42:18 +0000

    assign, show and menu functions

---
+++ src/client/show.py
@@ -0,0 +1,15 @@
+import user
+import computer
+import ports
+
+class show_values
+    def show_userInfo(self, user)
+            print ("user name : " , user.name , " user email: " user.email)
+
+    def show_computers (self, user)
+            print(computers)
+            
+    
+
+    
+                
